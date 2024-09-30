b70f015a439fdb5743bec0c8d1db3fb3e8d64db3
Mery182 <d182840@rambler.ru>
2024-10-01 00:02:10 +0300

    Добавление стилей в проекте Vue-Pizza

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="main__wrapper">
-    <div class="main__header">
-      <img src="@/assets/img/logo.svg" width="300" height="47" alt="V!U!E! Pizza" />
-    </div>
-    <h1>Добро пожаловать!</h1>
-    <p>
-      Это проект V!U!E! Pizza для обучения на профессиональном онлайн‑курсе<br />
-      <b>«Vue.js для опытных разработчиков».</b>
-    </p>
-  </div>
+  <app-layout>
+    <home-view />
+  </app-layout>
 </template>
+
+<script setup>
+import { AppLayout } from "@/layouts";
+import { HomeView } from "@/views";
+</script>
 
 <style lang="scss">
 @import "@/assets/scss/app.scss";
+
 body {
   justify-content: center;
   align-items: center;
@@ -59,4 +58,4 @@ body {
   }
 }
 
-</style>
+</style> 
