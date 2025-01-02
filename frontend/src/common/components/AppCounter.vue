9ec4e1a4f46987cdabfbda4a830e325961c9cdd3
Mery Efimova <d182840@rambler.ru>
2025-01-02 17:14:02 +0300

    l

--- frontend/src/common/components/AppCounter.vue
+++ frontend/src/common/components/AppCounter.vue
@@ -18,18 +18,18 @@ defineProps({
   },
 
 });
-
 const emit = defineEmits("input");
 </script>
+
 <template>
   <div class="counter ingredients__counter">
-    <button type="button" class="counter__button counter__button--minus " :disabled="value === min"
+    <button type="button" class="counter__button counter__button--minus" :disabled="value === min"
       @click="$emit('input', value - 1)">
       <span class="visually-hidden">Меньше</span>
     </button>
     <input type="text" name="counter" class="counter__input" :value="value"
       @input="$emit('input', Number($event.target.value))">
-    <button type="button" class="counter__button counter__button--plus" :class="{ 'counter__button--accent': accent }"
+    <button type="button" class="counter__button counter__button--plus" :class="{ 'counter__button--orange': accent }"
       :disabled="value === max" @click="$emit('input', value + 1)">
       <span class="visually-hidden">Больше</span>
     </button>
